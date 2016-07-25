78d7a91dae2316fca8feb901a0dff8cbaa25822c
Akhil Narang <akhilnarang.1999@gmail.com>
2016-07-25 20:51:26 +0530

    MTC19Z
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- cyanide.mk
+++ cyanide.mk
@@ -20,6 +20,7 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MTC19T/2741993:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MTC19T 2741993 release-keys" \
-    BUILD_ID=MTC19T
+    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MTC19Z/2996059:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MTC19Z 2996059 release-keys" \
+    BUILD_ID=MTC19Z
+
